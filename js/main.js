05102d77b6352335a817987f51b458e5dfe2cbf4
Derek <benakderek@seattleu.edu>
2018-06-30 21:53:31 +0000

    added additional let variables for userid and numbers

--- js/main.js
+++ js/main.js
@@ -87,9 +87,15 @@ let adjective5 = prompt('Give me another word to descrive your vehicle:');
 // TODO: To begin with, prompt the user to collect their favorite number that is not zero.
 // Call this variable "userNumber".
 
+let userNumber = prompt('What is your favorite number that is not zero?');
+
 // TODO: generate TWO more numbers called "number2" and "number 3".
 // Use any operator you would like to add, multiply, divide, or otherwise
 // change the number that the user submitted.
+
+let number2 = userNumber*5;
+
+let number3 = userNumber + number2 - 20;
 
 
 // DO NOT EDIT BELOW THIS LINE /////////////////////////////////////////
