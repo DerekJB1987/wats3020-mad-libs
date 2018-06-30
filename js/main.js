22d53f3cd4700200c29ab1c9cad3ead281f5068e
Derek <benakderek@seattleu.edu>
2018-06-30 23:14:56 +0000

    cleaned code in main Javascript file

--- js/main.js
+++ js/main.js
@@ -3,95 +3,59 @@
 // For this assignment you must fill in the code below so that it functions
 // to populate the text of the "Mad Libs" style story in the HTML.
 //
-// In order to achieve this goal, you will need to write prompt() statements
-// to collect input from the user. The input you collect will be used to
-// generate the story.
 //
 //
-// TODO: Write a prompt statement like the one below for each of the
-// variables listed.
+// Using the let variable and prompt() command, populate the following variables:
 
 let honorific = prompt('Please enter an honorific title (e.g. Mister or Colonel).');
 
-// Using the same prompt() command, populate the following variables:
-//
-// TODO: A variable named "authorName" for authorial attribution.
 let authorName = prompt('What is your full name?');
 
-// TODO: A variable named "adjective1" for a word used to describe something.
 let adjective1 = prompt('Give me a word to describe something:');
 
-// TODO: A variable named "vehicle" for a mode of transation (e.g. plane, boat, etc.).
 let vehicle = prompt('What is your preferred vehicle for transportation?');
 
-// TODO: A variable named "vehiclePart" for a part of the vehicle you just named.
 let vehiclePart = prompt('Name a part of your preferred vehicle:');
 
-// TODO: A variable named "parking" for a location where you park the vehicle you just named.
 let parking = prompt('Where do you park your vehicle?');
 
-// TODO: A variable named "precious" for a precious object.
 let precious = prompt('Name a precious object:');
 
-// TODO: A variable named "noiseMaker" for something that makes a sound.
 let noiseMaker = prompt('Name something that makes a sound:');
 
-// TODO: A variable named "crowdBehavior" for something that people do together as a group.
 let crowdBehavior = prompt('Name something that people do as a group:');
 
-// TODO: A variable named "adjective2" for a word used to describe something.
 let adjective2 = prompt('Give me a word to describe something:');
 
-// TODO: A variable named "adjective3" for a word used to describe something.
 let adjective3 = prompt('Give me a word to describe something:');
 
-// TODO: A variable named "color" for the name of a color.
 let color = prompt('Give me the name of a color:');
 
-// TODO: A variable named "familyMember" for the name of a family relationship (e.g. sister, father, etc.).
 let familyMember = prompt('What is a name of a family member?');
 
-// TODO: A variable named "organ" for the name of a body organ (e.g. heart, liver, spleen, etc.).
 let organ = prompt('What is the name of a body organ?');
 
-// TODO: A variable named "bodyPart1" for the name of a body part.
 let bodyPart1 = prompt('What is the name of a body part?');
 
-// TODO: A variable named "bodyPart2" for the name of a body part.
 let bodyPart2 = prompt('What is the name of another body part?');
 
-// TODO: A variable named "bodyPart3" for the name of a body part.
 let bodyPart3 = prompt('What is the name of another body part?');
 
-// TODO: A variable named "thing" for an object.
 let thing = prompt('Give me an object:');
 
-// TODO: A variable named "thing2" for an object you might give somebody you love.
 let thing2 = prompt('What is an object you might give somebody you love?');
 
-// TODO: A variable named "thing3" for an object you might give somebody you like.
 let thing3 = prompt('What is an object you might give somebody you like?');
 
-// TODO: A variable named "adjective4" for a word used to describe the vehicle.
 let adjective4 = prompt('Give me a word to describe your vehicle:')
 
-// TODO: A variable named "adjective5" for a word used to describe the vehicle.
 let adjective5 = prompt('Give me another word to describe your vehicle:');
 
-////////////////////////////////////////////////////////////////////////
-// Section Numbers
-//
-// The poem we are using has section numbers. Let's allow the user to
-// generate their own.
-//
-// TODO: To begin with, prompt the user to collect their favorite number that is not zero.
-// Call this variable "userNumber".
+// Using the let variable and prompt() command, populate the following variable titled userNumber:
 
 let userNumber = prompt('What is your favorite number that is not zero?');
 
-// TODO: generate TWO more numbers called "number2" and "number 3".
-// Use any operator you would like to add, multiply, divide, or otherwise
-// change the number that the user submitted.
+// Using the let variable and prompt() command, populate the following variable to create a number2 and number3 variables
 
 let number2 = userNumber*5;
 
